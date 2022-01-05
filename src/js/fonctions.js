d5f875837bab1ca7e20a71da0c23c9fe5d8cfd44
Thomas MEURIE <tommeurie@gmail.com>
2022-01-05 14:58:06 +0100

    finish CSS

--- src/js/fonctions.js
+++ src/js/fonctions.js
@@ -1,16 +1,33 @@
 function batmantransition() {
-    var batman = document.querySelector('.batman_superpose');
+    let bat = document.querySelector('.batman_acceuil-1');
     console.log('yes');
+    let O = bat.attributes.opacity
 
-    let transition = anime.timeline({
-        loop: false,
-        delay: 0
-    });
-
-    batman.add({
-        targets: batman_superpose,
-        opacity: [1,0],
+    return anime({
+        targets: '.batman_acceuil-1',
+        opacity: [O,1-O],
         duration: 300,
         easing: 'linear',
-    });
+        autoplay: false,
+    }).play;
+}
+
+function openmenu() {
+    return anime({
+        targets: '.menu',
+        translateY: 520,
+        autoplay: false,
+        easing: 'easeInOutSine',
+        duration: 300,
+    }).play;
+}
+
+function closemenu() {
+    return anime({
+        targets: '.menu',
+        translateY: 0,
+        autoplay: false,
+        easing: 'easeInOutSine',
+        duration: 300,
+    }).play;
 }
